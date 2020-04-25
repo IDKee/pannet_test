3eb71cb6fc82da59512c55ce1a8a9a138276c3b5
Administrator <354179482@qq.com>
2020-04-25 22:18:46 +0800

    修改

--- src/main/java/com/pan/common/constants/RedisConstants.java
+++ src/main/java/com/pan/common/constants/RedisConstants.java
@@ -9,11 +9,11 @@ public class RedisConstants {
      * shiro相关
      * shiro cache
      */
-    public static final String REDIS_SHIRO_CACHE = "dream-shiro-cache:";
+    public static final String REDIS_SHIRO_CACHE = "pannet-shiro-cache:";
 
     /**
      * shiro相关
      * shiro session
      */
-    public static final String REDIS_SHIRO_SESSION = "dream-shiro-session:";
+    public static final String REDIS_SHIRO_SESSION = "pannet-shiro-session:";
 }
